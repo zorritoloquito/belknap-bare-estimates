c0ccf490f1ee711ca10b7fca3c66be3dbc15f498
zorritoloquito <l_vellon@hotmail.com>
2025-05-15 02:38:03 -0700

    init db

---
+++ db/db.ts
@@ -0,0 +1,11 @@
+import { config } from "dotenv"
+import { drizzle } from "drizzle-orm/postgres-js"
+import postgres from "postgres"
+
+config({ path: ".env.local" })
+
+const schema = {}
+
+const client = postgres(process.env.DATABASE_URL!)
+
+export const db = drizzle(client, { schema })
